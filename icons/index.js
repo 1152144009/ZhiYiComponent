9274e97e2cabda26abd994b10f0ac00838cfcec8
caixm <cxm@Cai-XMs-Mac.local>
2019-07-18 16:43:59 +0800

    icons1

--- icons/index.js
+++ icons/index.js
@@ -1,77 +1,77 @@
 
 // 头像
-export let icon_avatar = require("../assets/image/icon_avatar.png")
+export let icon_avatar = require("./assets/image/icon_avatar.png")
 
 
 // 升序
-export let icon_asc = require("../assets/image/icon_asc.png")
+export let icon_asc = require("./assets/image/icon_asc.png")
 
 // 降序
-export let icon_desc = require("../assets/image/icon_desc.png")
+export let icon_desc = require("./assets/image/icon_desc.png")
 
 // 无序
-export let icon_disorder = require("../assets/image/icon_disorder.png")
+export let icon_disorder = require("./assets/image/icon_disorder.png")
 
 // 新增（绿色）
-export let icon_add_green = require("../assets/image/icon_add_green.png")
+export let icon_add_green = require("./assets/image/icon_add_green.png")
 
 // 返回(黑色)
-export let icon_back_black = require("../assets/image/icon_left_dark.png")
+export let icon_back_black = require("./assets/image/icon_left_dark.png")
 
 // 下拉列表（绿色）
-export let icon_select_green = require("../assets/image/icon_bottom_dark.png")
+export let icon_select_green = require("./assets/image/icon_bottom_dark.png")
 
 // 开票水印
-export let icon_cashier = require("../assets/image/icon_cashier.png")
+export let icon_cashier = require("./assets/image/icon_cashier.png")
 
 // 右箭头（灰色）
-export let icon_rightArrow_gray = require("../assets/image/icon_right.png")
+export let icon_rightArrow_gray = require("./assets/image/icon_right.png")
 
 // 右箭头（黑色）
-export let icon_rightArrow_black = require("../assets/image/icon_right.png")
+export let icon_rightArrow_black = require("./assets/image/icon_right.png")
 
 // 右箭头（白色）
-export let icon_rightArrow_white = require("../assets/image/icon_right_white.png")
+export let icon_rightArrow_white = require("./assets/image/icon_right_white.png")
 
 // 右箭头（黑色）
-export let icon_leftArrow_black = require("../assets/image/icon_left_dark.png")
+export let icon_leftArrow_black = require("./assets/image/icon_left_dark.png")
 
 // 左箭头（白色）
-export let icon_leftArrow_white = require("../assets/image/icon_left_white.png")
+export let icon_leftArrow_white = require("./assets/image/icon_left_white.png")
 
 // 下箭头（黑色）
-export let icon_list_open_detail = require("../assets/image/icon_list_open_detail.png")
+export let icon_list_open_detail = require("./assets/image/icon_list_open_detail.png")
 
 // 上箭头（黑色）
-export let icon_list_off_detail = require("../assets/image/icon_list_off_detail.png")
+export let icon_list_off_detail = require("./assets/image/icon_list_off_detail.png")
 // 搜索   (灰色）
-export let icon_public_search = require('../assets/image/icon_search_gray.png')
+export let icon_public_search = require('./assets/image/icon_search_gray.png')
 // 删除   (灰色)
-export let icon_public_delete = require('../assets/image/icon_public_delete.png')
+export let icon_public_delete = require('./assets/image/icon_public_delete.png')
 
 // 复选框选中
-export let icon_checkBox_green = require("../assets/image/icon_checkBox_green.png")
+export let icon_checkBox_green = require("./assets/image/icon_checkBox_green.png")
 
 // 单选框未选中
-export let icon_radio_gray = require("../assets/image/icon_radio_gray.png")
+export let icon_radio_gray = require("./assets/image/icon_radio_gray.png")
 
 // 单钻按钮选中
-export let icon_radio_green = require("../assets/image/icon_radio_green.png")
+export let icon_radio_green = require("./assets/image/icon_radio_green.png")
 
 // 地址图标
-export let icon_address = require("../assets/image/icon_address.png")
+export let icon_address = require("./assets/image/icon_address.png")
 
 // 购物车 框押金已收
-export let icon_cartlist_boxcashreceived = require("../assets/image/icon_cartlist_boxcashreceived.png")
+export let icon_cartlist_boxcashreceived = require("./assets/image/icon_cartlist_boxcashreceived.png")
 
 // 购物车 框押金未收
-export let icon_cartlist_boxcash = require("../assets/image/icon_cartlist_boxcash.png")
+export let icon_cartlist_boxcash = require("./assets/image/icon_cartlist_boxcash.png")
 
 //loading gif 图片
-export let icon_publick_loading = require("../assets/image/icon_publick_loading.gif")
+export let icon_publick_loading = require("./assets/image/icon_publick_loading.gif")
 
 //打印机loading gif 图片
-export let icon_publick_printerloading = require("../assets/image/icon_publick_printerloading.gif")
+export let icon_publick_printerloading = require("./assets/image/icon_publick_printerloading.gif")
 
 // 搜索图标蓝色
-export let icon_search_blue = require("../assets/image/icon_search_select.png")
+export let icon_search_blue = require("./assets/image/icon_search_select.png")
